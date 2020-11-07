dac090011fbd1eb409b7b2145e951a4921fc95e0
coderimba <56523608+tartwig@users.noreply.github.com>
2020-11-07 18:23:18 +0800

    Finished writing code for StudentOrganizerPanel class

--- src/main/ui/StudentOrganizerPanel.java
+++ src/main/ui/StudentOrganizerPanel.java
@@ -11,7 +11,9 @@ import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 // based on code written in components-ListDemoProject's components.ListDemo
 // as found in https://docs.oracle.com/javase/tutorial/uiswing/examples/components/index.html
@@ -25,18 +27,18 @@ public class StudentOrganizerPanel extends JPanel implements ListSelectionListen
     private static final String JSON_STORE = "./data/studentorganizer.json";
 
     private static final String markCompleteString = "Mark Complete";
-    private static final String viewIncompleteString = "View Incomplete";
+    private static final String deleteString = "Delete";
     private JButton markCompleteButton;
-    private JButton viewIncompleteButton;
+    private JButton deleteButton;
 
     public StudentOrganizerPanel() {
         super(new BorderLayout());
 
-        studentOrganizerModel = new DefaultListModel();
+        init();
         loadAssignments();
 
         studentOrganizerList = new JList(studentOrganizerModel);
-        studentOrganizerList.setSelectionModel(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        studentOrganizerList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         studentOrganizerList.setSelectedIndex(0);
         studentOrganizerList.addListSelectionListener(this);
         studentOrganizerList.setVisibleRowCount(5);
@@ -46,9 +48,9 @@ public class StudentOrganizerPanel extends JPanel implements ListSelectionListen
         markCompleteButton.setActionCommand(markCompleteString);
         markCompleteButton.addActionListener(new MarkCompleteListener());
 
-        viewIncompleteButton = new JButton(viewIncompleteString);
-        viewIncompleteButton.setActionCommand(viewIncompleteString);
-        viewIncompleteButton.addActionListener(new ViewIncompleteListener());
+        deleteButton = new JButton(deleteString);
+        deleteButton.setActionCommand(deleteString);
+        deleteButton.addActionListener(new DeleteListener());
 
         JPanel buttonPane = new JPanel();
         buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.LINE_AXIS));
@@ -56,16 +58,11 @@ public class StudentOrganizerPanel extends JPanel implements ListSelectionListen
         buttonPane.add(Box.createVerticalStrut(5));
         buttonPane.add(new JSeparator(SwingConstants.VERTICAL));
         buttonPane.add(Box.createVerticalStrut(5));
-        buttonPane.add(viewIncompleteButton);
+        buttonPane.add(deleteButton);
         buttonPane.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
 
         add(studentOrganizerScrollPane, BorderLayout.CENTER);
         add(buttonPane, BorderLayout.PAGE_END);
-    }
-
-    @Override
-    public void valueChanged(ListSelectionEvent e) {
-
     }
 
     public void loadAssignments() {
@@ -73,6 +70,15 @@ public class StudentOrganizerPanel extends JPanel implements ListSelectionListen
         for (Assignment a: myStudentOrganizer.viewAllAssignmentsByCourseCode()) {
             studentOrganizerModel.addElement(a);
         }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes DefaultListModel, StudentOrganizer, JsonWriter, and JsonReader objects
+    private void init() { // based on code written in Teller's ui.TellerApp init() method
+        studentOrganizerModel = new DefaultListModel();
+        myStudentOrganizer = new StudentOrganizer();
+        jsonWriter = new JsonWriter(JSON_STORE);
+        jsonReader = new JsonReader(JSON_STORE);
     }
 
     // MODIFIES: this
@@ -87,17 +93,57 @@ public class StudentOrganizerPanel extends JPanel implements ListSelectionListen
         }
     }
 
+    // EFFECTS: saves the Student Organizer to file
+    private void saveStudentOrganizer() { // based on code written in JsonSerializationDemo's ui.WorkRoomApp
+        // saveWorkRoom() method
+        try {
+            jsonWriter.open();
+            jsonWriter.write(myStudentOrganizer);
+            jsonWriter.close();
+            System.out.printf("Saved Student Organizer to %s\n", JSON_STORE);
+        } catch (FileNotFoundException e) {
+            System.out.printf("Unable to write to file: %s\n", JSON_STORE);
+        }
+    }
+
     private class MarkCompleteListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             int index = studentOrganizerList.getSelectedIndex();
-            studentOrganizerModel
+            Assignment assignment = (Assignment) studentOrganizerModel.elementAt(index);
+            assignment.markComplete();
         }
     }
 
-    private class ViewIncompleteListener implements ActionListener {
+    private class DeleteListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
+            int index = studentOrganizerList.getSelectedIndex();
+            studentOrganizerModel.remove(index);
+
+            int size = studentOrganizerModel.getSize();
+
+            if (size == 0) {
+                deleteButton.setEnabled(false);
+            } else {
+                if (index == studentOrganizerModel.getSize()) {
+                    index--;
+                }
+                studentOrganizerList.setSelectedIndex(index);
+                studentOrganizerList.ensureIndexIsVisible(index);
+            }
+        }
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        if (e.getValueIsAdjusting() == false) {
+
+            if (studentOrganizerList.getSelectedIndex() == -1) {
+                deleteButton.setEnabled(false);
+            } else {
+                deleteButton.setEnabled(true);
+            }
 
         }
     }
